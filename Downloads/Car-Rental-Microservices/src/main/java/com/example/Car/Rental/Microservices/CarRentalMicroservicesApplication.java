f4e9ea0da55db6b205bf9c75d6257c054c40bbb8
Rodion Rybko <rybkorodion201@mail.ru>
2025-06-20 12:07:41 +0700

    commit

---
+++ Downloads/Car-Rental-Microservices/src/main/java/com/example/Car/Rental/Microservices/CarRentalMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Car.Rental.Microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalMicroservicesApplication.class, args);
+	}
+
+}
---
+++ Downloads/Car-Rental-Microservices/src/test/java/com/example/Car/Rental/Microservices/CarRentalMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Car.Rental.Microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
